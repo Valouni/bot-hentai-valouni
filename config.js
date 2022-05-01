3129d5a506b22c488ca95feafb5d42e5b814e426
Valouni <batteurxy@gmail.com>
2022-05-01 21:10:36 +0200

    Ajout du code du bot

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  prefix: '$',
+
+  arrivé: '922467584480256060',
+  role: '922513176245968906',
+  ticket: '970234408374247434',
+  catégo: '970233100112117770',
+  catégoarchive: '970233759297318922',
+  rolereact: '969852619675795466',
+  roleadmin: '777576838678577172',
+  everyone: '922463995871502346',
+  token: 'OTcwMjI5NjUwMDYyMTg4NTc1.Ym46xg.KtsFg65ujSTJ_gn8E56hm-7NWAc'
+}
